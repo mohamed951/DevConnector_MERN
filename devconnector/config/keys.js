d1d82add8da7f83f638435352505fac6871f15d3
mohamed951 <mohamedshawkat95@gmail.com>
2019-05-25 14:52:39 +0200

    Inilization express/mongoDB/routes

---
+++ devconnector/config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://root:root@cluster0-04ejn.mongodb.net/test?retryWrites=true'
+}
---
+++ devconnector/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+const app = express();
+
+//DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+    .connect(db, {
+        useNewUrlParser: true
+    })
+    .then(() => {
+        console.log("Database Connected")
+    })
+    .catch((err) => {
+        console.log(`Database Error ${err}`)
+    })
+
+app.get('/', (req, res) => res.send("hello"))
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log("Server Run")
+})
